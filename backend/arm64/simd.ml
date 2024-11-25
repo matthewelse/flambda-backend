0dfe346974cee92cc21a107b46957e1b4a628a7a
Matthew Else <matthewelse1997@gmail.com>
2024-11-25 07:20:40 +0000

    arm64: codegen for float32 rounding

--- backend/arm64/simd.ml
+++ backend/arm64/simd.ml
@@ -27,7 +27,14 @@ type int_datatype =
   | Int32x4
   | Int64x2
 
-type operation = Add of int_datatype
+type rounding_mode =
+  | Neg_inf
+  | Pos_inf
+  | Zero
+
+type operation =
+  | Add of int_datatype
+  | Round_f32 of rounding_mode
 
 let reg_suffix_for_int_datatype dt =
   match dt with
@@ -46,12 +53,28 @@ let print_int_datatype ppf dt =
 let printreg_dt dt printreg ppf reg =
   fprintf ppf "%a.%a" printreg reg print_int_datatype dt
 
+let rounding_mode_instruction_suffix = function
+  | Neg_inf -> "m"
+  | Pos_inf -> "p"
+  | Zero -> "z"
+
+let print_rounding_mode ppf mode =
+  let suffix = rounding_mode_instruction_suffix mode in
+  Format.pp_print_string ppf suffix
+
 let print_operation printreg op ppf arg =
   match op with
   | Add dt ->
     fprintf ppf "add%a %a %a" print_int_datatype dt (printreg_dt dt printreg)
       arg.(0) (printreg_dt dt printreg) arg.(1)
+  | Round_f32 mode ->
+    fprintf ppf "frint%a %a %a" print_rounding_mode mode printreg arg.(0)
+      printreg arg.(1)
 
-let equal_operation op1 op2 = match op1, op2 with Add dt, Add dt' -> dt = dt'
+let equal_operation op1 op2 =
+  match op1, op2 with
+  | Add dt, Add dt' -> dt = dt'
+  | Round_f32 mode, Round_f32 mode' -> mode = mode'
+  | Add _, _ | Round_f32 _, _ -> false
 
-let operation_is_pure op = match op with Add _ -> true
+let operation_is_pure op = match op with Add _ -> true | Round_f32 _ -> true
